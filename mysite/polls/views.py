67943f343730c3e833645a02481917cb8beec131
agarwalshivani23 <agarwalshivani23@gmail.com>
2021-03-24 18:38:26 +0530

    django project

---
+++ mysite/polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader,RequestContext
+from django.shortcuts import render
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+def detail(request,question_id):
+    return HttpResponse("This is the detail view of question: %s" % question_id)
+def results(request,question_id):
+    return HttpResponse("Tehese are the result: %s " % question_id)
+def vote(request,question_id):
+    return HttpResponse("Vote on questions: %s" % question_id)
